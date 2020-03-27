7faec6849c0fc9dc6ec578e7266ea4031564cedd
Luis Caraballo <luisred61@gmail.com>
2020-03-27 12:44:31 -0500

    Project started: Basic Configuration with DB mock

---
+++ store/dummy.js
@@ -0,0 +1,32 @@
+const db = {
+    'user': [
+        {
+            id: 1,
+            name: 'Carlos'
+        }
+    ]
+};
+
+function list(table) {
+    return db[table]
+}
+
+function get(table, id) {
+    let collection = list(table);
+    return collection.filter(item => item.id === id)[0] || null;
+}
+
+function upsert(table, data) {
+    db[collection].push(data)
+}
+
+function remove(table, id) {
+    return true;
+}
+
+module.exports = {
+    list,
+    get,
+    upsert,
+    remove
+}
